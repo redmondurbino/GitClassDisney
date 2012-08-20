f070c54eafa10e031d6ec1f2c59c3c826300f5e6
Redmond A. Urbino <rurbino@kaspar>
2012-08-20 03:16:19 -0700

    made starting life 6

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class HelloWorld : MonoBehaviour {
 	
-	private int StartingLife = 4;
+	private int StartingLife = 6;
 	// Use this for initialization
 	void Start () {
 	
