216194e322b22fe15a704a5d3def1ce3a281a4eb
Redmond Urbino <rurbino@kaspar>
2012-08-20 03:03:20 -0700

    change startling life to 2

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class HelloWorld : MonoBehaviour {
 	
-	private int StartingLife = 1;
+	private int StartingLife = 2;
 	// Use this for initialization
 	void Start () {
 	
